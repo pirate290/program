73b5548ae992bdf6c8a29d9c2551d73af304071f
pirate290 <abdoulkaderake@gmail.com>
2022-09-20 12:42:07 +0000

    main.c modifier

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - momo mobile
+* return - always wero
+*/
+  int add (int a, int b);
+int main (void)
+{
+    int a , b;
+
+
+
+}
+
+int add (int a , int b)
+{
+    int result;
+    result = a + b;
+
+    return (0);
+}
